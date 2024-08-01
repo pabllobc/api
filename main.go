c205427d98ed7b1ba5980bbd7a9c3182c76cc2d9
Pabllo Borges Cardoso <pabllobc@gmail.com>
2024-08-01 11:11:22 -0300

    Criação de rota raiz e buscando todas as tarefas

--- main.go
+++ main.go
@@ -6,12 +6,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Tasks struct {
+	Id    int    `json:"id`
+	Title string `json:"title`
+}
+
+var taskList = []Tasks{
+	{Id: 1, Title: "Task 1"},
+	{Id: 2, Title: "Task 2"},
+}
+
 func main() {
 	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
+
+	//Rota de teste raiz
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Minha primeira API funcionando!",
 		})
 	})
-	router.Run(":3000")
+
+	//Buscando todas as tarefas
+	router.GET("/tarefas", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, taskList)
+	})
+
+	router.Run(":3001")
 }
